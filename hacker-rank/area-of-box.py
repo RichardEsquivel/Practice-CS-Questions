5fb1e1449c424efa50d40eba68bd873fa7a19f36
RichardEsquivel <rlesquivel@gmail.com>
2020-05-27 17:22:49 -0500

    worked through area of the box

---
+++ hacker-rank/area-of-box.py
@@ -0,0 +1,19 @@
+# recognize the pattern, formulate code, check for edge cases
+
+# number of ways to arrange cables of a certain bundle dimension
+def numberWays(dimensions):
+# This represents the sum total of how many number of ways we can arrange a box of certain dimensions with certain bundle sizes 2x3 box has 6 ways to place 1x1 bundles 2 ways to place 2x2 bundle
+    totalWays = []
+
+    for (r, c) in ways:
+        total = 0
+
+        while r > 0 and c > 0:
+            total += r * c
+		        r -= 1
+            c -= 1
+
+        totalWays.append(total)
+
+    return totalWays
+
