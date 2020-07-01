2eca1e614616b5050408e9f097d06ea82b079701
RichardEsquivel <rlesquivel@gmail.com>
2020-06-30 23:14:52 -0500

    Added React conceptual Notes created today.

--- jsconceptnotes.js
+++ jsconceptnotes.js
@@ -231,3 +231,136 @@ const reduceStatePopulations = data.reduce((total, state) => {
   return total += state.population;
 }, 0);
 ```
+## How React works under the hood:
+
+React is a JavaScript library and not strictly a framework like .NET or Angular or Ruby On Rails. You import React like any JavaScript library and you use its methods like React.useState and ReactDom. React is an entire network of tools working together like
+
+- React, npm, node, webpack, jsx, babel (postcss and eslint)
+
+## JSX
+
+https://reactjs.org/docs/introducing-jsx.html
+
+Allows you to put HTML in your JavaScript code and allows you to put JavaScript code in your HTML.
+
+Example below we declare a variable  called name and then can use it in jsx by wrapping it in { } braces
+
+JSX is an expression to so you can use it inside of regular JavaScript functions. So you can use it inside of if statements and for loops(assign it to variables, accept as arguments and return it from functions)
+
+Quotes used to specify string literals and curly braces for a javascript expression within an attribute. *JSX uses camel case naming conventions. 
+
+- JSX transpiles to regular JavaScript code
+- JSX allows you to put HTML in your JavaScript and allows you to put JavaScript code in your HTML. Both as expressions.
+
+```jsx
+import React, {useState, useEffect, useContext} from 'react';
+
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+```
+
+Watch out for ASI or automatic semicolon insertion when separating cohesive logical units and breaking them up with line breaks. 
+
+[http://www.ecma-international.org/ecma-262/7.0/index.html#sec-rules-of-automatic-semicolon-insertion](http://www.ecma-international.org/ecma-262/7.0/index.html#sec-rules-of-automatic-semicolon-insertion)
+
+Separations of concerns within React allow us to increase freedom for modules to do singular functions and maintenance of code only requires updating and reworking of that particular module. Also makes testing easier for each unit.
+
+## Types in JavaScript:
+
+Design ideas from Java and C, not concerned with input and output, relies on being hosted for input and output to be supplied.
+
+Number
+
+- Number
+- String
+- Boolean
+- Function
+- Object
+- Symbol(new in ES2015)
+- undefined and Null(changes were brought to ES2020)
+- Array(special kind of object)
+
+Must be accessed with index and not guaranteed to be dense, data can be stored at non-contiguous positions. May move to typed arrays
+
+[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
+
+- Date date is like the Unit Epoch Time but in milliseconds and
+- RegExp
+
+```
+/ab+c/i
+new RegExp(/ab+c/, 'i') // literal notation
+new RegExp('ab+c', 'i') // constructor use when in a loop and it will change
+```
+
+## Security in JSX and in general Cross site scripting prevention:
+
+https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html
+
+https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#xss-prevention-rules
+
+[https://en.wikipedia.org/wiki/Cross-site_scripting#:~:text=Cross-site scripting (XSS),pages viewed by other users](https://en.wikipedia.org/wiki/Cross-site_scripting#:~:text=Cross%2Dsite%20scripting%20(XSS),pages%20viewed%20by%20other%20users).
+
+*Security it is safer to embed user input in JSX, by default React Dom escapes an values embedded in JS before rendering them.  So if someone attempts to inject JavaScript within an HTML field. Everything is converted to a string before being rendered.
+
+If you insist on putting untrusted data into nested contexts, please do a lot of cross-browser testing and let us know what you find out.
+
+Don't do this or accept input as this:
+
+Directly in a script:
+
+`<script>...NEVER PUT UNTRUSTED DATA HERE...</script>`
+
+Inside an HTML comment:
+
+`<!--...NEVER PUT UNTRUSTED DATA HERE...-->`
+
+In an attribute name:
+
+`<div ...NEVER PUT UNTRUSTED DATA HERE...=test />`
+
+In a tag name:
+
+`<NEVER PUT UNTRUSTED DATA HERE... href="/test" />`
+
+Directly in CSS:
+
+`<style>
+...NEVER PUT UNTRUSTED DATA HERE...
+</style>`
+
+Most importantly, never accept actual JavaScript code from an untrusted source and then run it. For example, a parameter named "callback" that contains a JavaScript code snippet. No amount of escaping can fix that.
+
+## JSX Represents Objects
+
+Babel compiles JSX down to React.createElement() calls. These two are identical expressions:
+
+```jsx
+const element = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>);
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+
+//React creates these elements and uses them to to construct the DOM 
+//and keep it up to date
+
+// Note: this structure is simplified but React.createElement basically creates this
+//and uses these to construct the DOM, known as React elements.
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
+```
