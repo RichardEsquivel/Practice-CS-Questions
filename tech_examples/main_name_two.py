9b0c9a36dd0c5f880df5b378b1d9bc1f3538a74b
RichardEsquivel <rlesquivel@gmail.com>
2020-07-18 17:21:00 -0500

    worked through randomly shuffling through dictionary object with keys and values of lists

--- tech_examples/main_name_two.py
+++ tech_examples/main_name_two.py
@@ -1,4 +1,18 @@
-import main_name_one
+# import main_name_one
 
 
-print("I am main_name_two:", __name__)
+# print("I am main_name_two:", __name__)
+import random
+
+dictionary = {
+    "tennis": ["her", "two", "lemons", "lemons", "frog"],
+    "to": ["frogs", "frogs", "apples", "baked",],
+}
+first_word = [word for word in dictionary.keys()]
+
+for _ in range(6):
+    word = random.choice(first_word)
+    print(word, end="  ")
+    choose_next = dictionary[word]
+    word = random.choice(choose_next)
+    print(word, end="  ")
